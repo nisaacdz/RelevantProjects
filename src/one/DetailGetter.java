296ecbb5858a8509d67cf6c8cd9e3b165c099526
nisaacdz <103804474+nisaacdz@users.noreply.github.com>
2022-07-24 01:44:33 +0000

    Created

---
+++ src/one/DetailGetter.java
@@ -0,0 +1,9 @@
+package one;
+
+public class DetailGetter {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/one/Test.java
@@ -0,0 +1,11 @@
+package one;
+
+import javax.xml.stream.events.Characters;
+
+public class Test {
+
+	public static void main(String[] args){
+		System.out.println(Characters.CHARACTERS);
+	}
+	
+}
